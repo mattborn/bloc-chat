2104fc593b8a1e90cbdc8f0a2dddd427e529829c
abontia <abontia@imanet.org>
2016-09-22 13:30:08 +0000

    Added code to scroll last message above the input box and clear input Box after submission

--- src/Form.js
+++ src/Form.js
@@ -4,6 +4,7 @@ const Form = React.createClass({
 
   submit(e) {
     e.preventDefault();
+
     const text = this.refs.text.value;
     if (text.trim()) {
       this.props.messagesRef.push({
@@ -15,16 +16,29 @@ const Form = React.createClass({
           uid: this.props.user.uid,
         },
       });
+        // SCROLL MESSAGE ABOVE INPUT BOX ==
+        window.setTimeout(function(){
+		var body = document.body,
+		html = document.documentElement;
+		var height = Math.max( body.scrollHeight, body.offsetHeight,
+		html.clientHeight, html.scrollHeight, html.offsetHeight );
+		height = height + 2000;
+		window.scroll(0,height);
+                // CLEAR INPUT BOX UPON SEND ==
+                document.getElementById("inputBox").value="";
+        },500);
+
     }
   },
 
   render() {
     return (
       <form className="form" onSubmit={this.submit}>
-        <input ref="text"/>
-        <button>Send</button>
+        <input id="inputBox" ref="text"/>
+        <button onClick="clearField">Send</button>
       </form>
     );
+
   }
 });
 export default Form;
